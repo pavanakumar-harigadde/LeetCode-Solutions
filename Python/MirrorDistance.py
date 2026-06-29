3e73e74f6e0338e8d2f09ceb57fc9d8bd8873cdb
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-06-29 20:13:54 +0530

    ADD : MirrorDistance.py
    Added a python function solution to a leetcode problem.
    3783. Mirror Distance of an Integer

---
+++ Python/MirrorDistance.py
@@ -0,0 +1,15 @@
+#3783. Mirror Distance of an Integer
+
+int reverse(int n){
+    int rev=0;
+    while(n>0){
+        int rem = n%10;
+        rev = (rev*10)+rem;
+        n=n/10;
+    }
+    return rev;
+}
+
+int mirrorDistance(int n) {
+    return abs(n-reverse(n));
+}
