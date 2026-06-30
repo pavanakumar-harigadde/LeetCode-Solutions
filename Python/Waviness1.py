f4cb8ca41edf4dfa304612200d1bdd1c4d2846dc
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-06-30 18:54:03 +0530

    ADD : Waviness1.py
    Added a python function solution given to a leetcode problem.
    
    3751. Total Waviness of Numbers in Range I.

---
+++ Python/Waviness1.py
@@ -0,0 +1,10 @@
+#3751. Total Waviness of Numbers in Range I
+
+int totalWaviness(int num1, int num2) {
+    if(num2<100){
+        return 0;
+    }
+    for(int i=num1; i<=num2; i++){
+        for
+    }
+}
