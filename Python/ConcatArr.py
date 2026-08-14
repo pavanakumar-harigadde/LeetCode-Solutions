bd3ff98a7d9b242214be398bc4b2e15b9d8614ee
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-08-14 07:13:56 +0530

    ADD : ConcatArr.py
    Added a solution to a leetcode problem in python.
    1929. Concatenation of Array

---
+++ Python/ConcatArr.py
@@ -0,0 +1,10 @@
+#1929. Concatenation of Array
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return 2*nums
+        #(nums+ nums) also accepted
